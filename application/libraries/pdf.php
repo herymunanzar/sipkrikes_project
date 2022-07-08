575b5381c16b87dbccee6d43ea33bdcb6bd4de65
herymunanzar <88892115+herymunanzar@users.noreply.github.com>
2022-07-09 01:25:19 +0700

    update huruf tcpdf

--- application/libraries/pdf.php
+++ application/libraries/pdf.php
@@ -3,7 +3,7 @@ if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 require_once dirname(__FILE__).'/tcpdf/tcpdf.php';
 
-class pdf extends TCPDF
+class Pdf extends TCPDF
 {
     function __construct()
     {
