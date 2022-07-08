7cb200c7aa9ad00807da57213b03189bc6378a94
herymunanzar <88892115+herymunanzar@users.noreply.github.com>
2022-07-09 00:35:02 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sipkrikes_project/';
+$config['base_url'] = 'http://localhost:8080/sipkrikes_project/';
 
 /*
 |--------------------------------------------------------------------------
